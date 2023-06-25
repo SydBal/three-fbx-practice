6f8da22cb8f419607c2cc8fbd04a43859887ff52
Dominic Balassone <dominic.balassone@gmail.com>
2023-06-24 21:51:33 -0700

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+    // Configure base to your GitHub repo name
+    base: "vite-gh-pages",
+})
